50fcd3c50db977c8c99898dbbb3be14be4ea75bb
Kleiton Lima <kleiton.mini@gmail.com>
2022-03-16 11:50:29 -0300

    Versão 1.0

--- M1 - Projeto 02 - Jokenpô.js
+++ M1 - Projeto 02 - Jokenpô.js
@@ -1,12 +1,12 @@
 console.clear();
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 
 let pedra, papel, tesoura, escolhaU, escolhaIA;
 
-while(isNaN(escolhaU)) {
-    escolhaU = +prompt(`Digite sua escolha: (1) PEDRA (2) PAPEL (3) TESOURA`);
-    escolhaIA = Math.floor(Math.random() * 3 + 1);
-};
+while (isNaN(escolhaU)) {
+  escolhaU = +prompt(`Digite sua escolha: (1) PEDRA (2) PAPEL (3) TESOURA`);
+  escolhaIA = Math.floor(Math.random() * 3 + 1);
+}
 
 console.log(escolhaU);
 console.log(escolhaIA);
