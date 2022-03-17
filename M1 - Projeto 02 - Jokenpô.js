98143aa9795efe0890f060d9586432a566af57d3
Kleiton Lima <kleiton.mini@gmail.com>
2022-03-17 00:40:22 -0300

    Uptade

--- M1 - Projeto 02 - Jokenpô.js
+++ M1 - Projeto 02 - Jokenpô.js
@@ -7,8 +7,4 @@ while (isNaN(escolhaU)) {
   escolhaU = +prompt(`Digite sua escolha: (1) PEDRA (2) PAPEL (3) TESOURA`);
   escolhaIA = Math.floor(Math.random() * 3 + 1);
 }
-
-console.log(escolhaU);
-console.log(escolhaIA);
-
 console.log();
