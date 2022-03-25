69999c916d129c48a179f5cf2bc9de89c589465c
Kleiton Lima <kleiton.mini@gmail.com>
2022-03-25 19:14:13 -0300

    Update

--- M1 - Projeto 03 - Jogo de Ficção Interativa .js
+++ M1 - Projeto 03 - Jogo de Ficção Interativa .js
@@ -3,3 +3,29 @@ const prompt = require("prompt-sync")();
 
 // PROJETO NÃO FINALIZADO
 
+let personagem = {
+  forca: 3,
+  resistencia: 3,
+  energia: 10,
+  ataque: function () {
+    return this.forca * this.energia;
+  },
+  defesa: function () {
+    return this.resistencia * this.energia;
+  },
+};
+let espada = personagem.ataque() * 100;
+
+let dragao = {
+  forca: 30,
+  resistencia: 30,
+  energia: 100,
+  ataque: function () {
+    return this.forca * this.energia;
+  },
+  defesa: function () {
+    return this.resistencia * this.energia;
+  },
+};
+
+console.log();
