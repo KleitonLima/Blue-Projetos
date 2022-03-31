8b534e2fdf07245fa77cb2781aaea9bfeffb2683
Kleiton Lima <kleiton.mini@gmail.com>
2022-03-31 16:44:23 -0300

    Update 31/3

--- M1 - Projeto 03 - Jogo de Ficção Interativa .js
+++ M1 - Projeto 03 - Jogo de Ficção Interativa .js
@@ -5,12 +5,13 @@ const prompt = require("prompt-sync")();
 
 // JOGO DE FICÇÃO INTERATIVA
 
-// VARIAVÉIS
+// VARIÁVEIS
 let destino,
   emboscada,
   escolha,
   jogada,
   quantidadeInimigos,
+  manada,
   princesa = 0,
   livera = 0,
   missera = 0,
@@ -25,8 +26,8 @@ let tempo = {
 let personagem = {
   forca: 3,
   resistencia: 3,
-  energia: 20,
-  vida: 10,
+  energia: 10,
+  vida: 15,
   ataque: () => {
     return this.forca * this.energia;
   },
@@ -51,37 +52,6 @@ let dragao = {
 };
 
 // FUNÇÕES
-mostraStatus = () => {
-  if (tempo.hora >= 24) {
-    (tempo.hora -= 24), tempo.dia++;
-  } else if (tempo.minuto >= 60) {
-    (tempo.minuto -= 60), tempo.hora++;
-    console.log(`Vida: ${personagem.vida}`);
-  }
-  console.log(`Dia ${tempo.dia} - ${tempo.hora}h:${tempo.minuto}m`);
-};
-mostraTempo = () => {
-  if (tempo.hora >= 24) {
-    (tempo.hora -= 24), tempo.dia++;
-  } else if (tempo.minuto >= 60) {
-    (tempo.minuto -= 60), tempo.hora++;
-    console.log(`Vida: ${personagem.vida}`);
-  }
-};
-descanso = () => {
-  if (personagem.vida < 15) {
-    do {
-      console.log(`Você deseja descansar?`);
-      escolha = prompt().toLowerCase();
-      console.clear();
-      if (escolha == `s` || escolha == `sim`) {
-        passaHora(15 - personagem.vida);
-        personagem.vida = 15;
-        console.clear();
-      }
-    } while (escolha != `s` && escolha != `n` && escolha != `sim` && escolha != `nao`);
-  }
-};
 
 perdeVida = (dano) => {
   return (personagem.vida -= dano);
@@ -102,46 +72,88 @@ dado = () => {
   return Math.floor(Math.random() * 6 + 1);
 };
 continuar = () => {
+  console.log();
   prompt(`Pressione ENTER para continuar...`);
 };
-
+gameOver = () => {
+  console.log();
+  personagem.vida = 0;
+  mostraVida();
+  console.log();
+  console.log(`#GAME OVER#`);
+  console.log();
+};
+mostraStatus = () => {
+  console.clear();
+  if (tempo.hora >= 24) {
+    (tempo.hora -= 24), tempo.dia++;
+  } else if (tempo.minuto >= 60) {
+    (tempo.minuto -= 60), tempo.hora++;
+  }
+  console.log(`Dia ${tempo.dia} - ${tempo.hora}h:${tempo.minuto}m`);
+  console.log(`Vida: ${personagem.vida}`);
+};
+mostraTempo = () => {
+  console.clear();
+  if (tempo.hora >= 24) {
+    (tempo.hora -= 24), tempo.dia++;
+  } else if (tempo.minuto >= 60) {
+    (tempo.minuto -= 60), tempo.hora++;
+    console.log(`Vida: ${personagem.vida}`);
+  }
+};
+descanso = () => {
+  if (personagem.vida < 15) {
+    do {
+      console.clear();
+      mostraStatus();
+      console.log(`Você deseja descansar?`);
+      escolha = prompt().toLowerCase();
+      console.clear();
+      if (escolha == `s` || escolha == `sim`) {
+        passaHora(15 - personagem.vida);
+        personagem.vida = 15;
+        console.clear();
+      }
+    } while (escolha != `s` && escolha != `n` && escolha != `sim` && escolha != `nao`);
+  }
+};
 escolhaDestino = () => {
   do {
     console.clear();
     mostraStatus();
     console.log(`Por onde você deseja ir? `);
-    let escolhaDestino = [, `(1) Resgatar a príncesa Arlim`, `(2) Roubar a espada Lívera`, `(3) Enfrentar o dragão Míssera`, `(4) Buscar a pedra mística de Labuge`, `(5) Dormir`];
+    let listaDestino = [, `(1) Resgatar a princesa Arlim`, `(2) Roubar a espada Lívera`, `(3) Enfrentar o dragão Míssera`, `(4) Buscar a pedra mística de Labuge`, `(5) Dormir`, , , , `(9) Desistir`];
+
     if (princesa < 1) {
-      console.log(escolhaDestino[1]);
+      console.log(listaDestino[1]);
     }
     if (livera < 1) {
-      console.log(escolhaDestino[2]);
+      console.log(listaDestino[2]);
     }
     if (missera < 1) {
-      console.log(escolhaDestino[3]);
+      console.log(listaDestino[3]);
     }
     if (labuge < 1) {
-      console.log(escolhaDestino[4]);
+      console.log(listaDestino[4]);
     }
     if (tempo.hora >= 17 || tempo.hora < 5) {
-      console.log(escolhaDestino[5]);
+      console.log(listaDestino[5]);
     }
+    console.log(listaDestino[9]);
     destino = +prompt();
-  } while (isNaN(destino) || !Number.isInteger(destino) || destino < 1 || destino > 5);
+  } while (isNaN(destino) || !Number.isInteger(destino) || destino < 1 || destino > 9);
 };
 
 // ESCOLHA DO DESTINO
 while (true) {
   escolhaDestino();
-  if (destino == 5) {
-    (tempo.hora = 5), (tempo.minuto = Math.floor(Math.random() * 59 + 1)), tempo.dia++;
-  }
 
   // DESTINO 1
   if (destino == 1) {
     console.clear();
     mostraTempo();
-    console.log(`Muito bem! Siga por este caminho e chegará até o cativeiro da príncesa. Cuidado no caminho e boa sorte!`);
+    console.log(`Muito bem! Siga por este caminho e chegará até o cativeiro da princesa. Cuidado no caminho e boa sorte!`);
     console.log();
     continuar();
 
@@ -171,9 +183,7 @@ while (true) {
             i--;
             mostraStatus();
             console.log(`Você derrotou um inimigo, falta ${i}`);
-            perdeEnergia(1);
           } else {
-            perdeEnergia(1);
             perdeVida(2);
             mostraStatus();
             console.log(`O inimigo defendeu seu ataque e contra atacou!`);
@@ -181,7 +191,6 @@ while (true) {
         }
         if (escolha == 2) {
           if (jogada >= emboscada) {
-            perdeEnergia(1);
             mostraStatus();
             console.log(`Você defendeu o ataque inimigo.`);
           } else {
@@ -192,17 +201,13 @@ while (true) {
         }
         if (personagem.vida <= 0) {
           console.clear();
-          console.log(`Você foi morto!`);
-          console.log();
-          console.log(`#GAME OVER#`);
+          gameOver();
           break;
         }
       }
       if (personagem.vida <= 0) {
         console.clear();
-        console.log(`Você foi morto!`);
-        console.log();
-        console.log(`#GAME OVER#`);
+        gameOver();
         break;
       }
 
@@ -222,7 +227,7 @@ while (true) {
       console.clear();
       mostraStatus();
       console.log(`Você chegou ao cativeiro! Você contou cerca de 15 soldados Argadans. Como deseja agir?`);
-      console.log(`(1) DEIXAR SER PRESO E PLANEJAR UMA FULGA COM A PRINCESA NA PRISÃO`);
+      console.log(`(1) DEIXAR SER PRESO E PLANEJAR UMA FUGA COM A PRINCESA NA PRISÃO`);
       console.log(`(2) ATACAR OS GUARDAS`);
       console.log(`(3) INVADIR SORRATEIRAMENTE ATÉ ENCONTRAR A PRINCESA`);
       escolha = +prompt();
@@ -241,12 +246,7 @@ while (true) {
       console.clear();
       mostraTempo();
       console.log(`Você travou um intensa batalha contra os Argadans, mas eles eram muitos e você acabou morto!`);
-      console.log();
-      personagem.vida = 0;
-      mostraVida();
-      console.log();
-      console.log(`#GAME OVER#`);
-      console.log();
+      gameOver();
       break;
     } else {
       jogada = dado();
@@ -290,14 +290,67 @@ while (true) {
           passaHora(1);
           mostraTempo();
           console.log(`Você travou uma intensa batalha mas, eles eram muitos e você foi morto!`);
-          console.log();
-          personagem.vida = 0;
-          console.log();
-          console.log(`#GAME OVER#`);
-          console.log();
+          gameOver();
           break;
         }
       }
     }
+  }
+
+  // DESTINO 2
+  else if (destino == 2) {
+    mostraStatus();
+    console.log(`Perfeito! Desça por esse caminho e chegará a base dos Argadans, onde a espada está localizada.`);
+    continuar();
+    manada = dado();
+    if (manada % 2 == 0) {
+      mostraStatus();
+      console.log(`Enquanto atravessava um imenso vale, você avistou uma manada de Orlus, animais maiores que elefantes.`);
+      console.log(`Você precisa decidir como agir...`);
+      continuar();
+      mostraStatus();
+      console.log(`Pode tentar correr mais que os Orlus e voltar ao inicio do vale, à cerca de 1km...`);
+      console.log(`Pode se esconder atrás de uma enorme pedra no meio do vale....`);
+      console.log(`Subir em uma árvore também no meio do vale...`);
+      console.log();
+      console.log(`(1) CORRER PARA O INICIO DO VALE`);
+      console.log(`(2) ESCONDER ATRÁS DA PEDRA`);
+      console.log(`(3) SUBIR NA ÁRVORE`);
+      escolha = +prompt();
+      if (escolha == 1) {
+        addminuto(10);
+        mostraTempo();
+        console.log(`Você correu muito rápido... só não, mais rápido que a manada que te pisoteou.`);
+        gameOver();
+        break;
+    }
+  }
+
+  // DESTINO 3
+  else if (destino == 3) {
+    if (livera == 0 && labuge == 0) {
+      do {
+        mostraStatus();
+        console.log(`Você não terá chance contra o dragão Míssera sem a espada Lívera e a pedra Labuge.`);
+        escolha = prompt(`Deseja mesmo continuar? `).toLowerCase();
+      } while (escolha != `s` && escolha != `n` && escolha != `sim` && escolha != `nao`);
+      if (escolha == `s` || escolha == `sim`) {
+        mostraStatus();
+        console.log(`Tudo bem! Mas, não diga que não foi avisado!`);
+        console.log(`Suba por este caminho e chegará a caverna do dragão Míssera! Boa sorte!`);
+        continuar();
+      }
+    }
+    console.log();
+  }
+
+  // DESTINO 5
+  else if (destino == 5) {
+    (tempo.hora = 5), (tempo.minuto = Math.floor(Math.random() * 59 + 1)), tempo.dia++;
+  }
+
+  // DESTINO 9
+  else if (destino == 9) {
+    break;
   }
 }
