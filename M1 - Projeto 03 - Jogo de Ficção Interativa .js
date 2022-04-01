ee8ddd04760de09a4e252f2572e50ef9e6da1f52
Kleiton Lima <kleiton.mini@gmail.com>
2022-04-01 01:08:28 -0300

    Update Destino 2 pronto

--- M1 - Projeto 03 - Jogo de Ficção Interativa .js
+++ M1 - Projeto 03 - Jogo de Ficção Interativa .js
@@ -12,7 +12,7 @@ let destino,
   jogada,
   quantidadeInimigos,
   manada,
-  princesa = 0,
+  arlim = 0,
   livera = 0,
   missera = 0,
   labuge = 0;
@@ -99,8 +99,8 @@ mostraTempo = () => {
     (tempo.hora -= 24), tempo.dia++;
   } else if (tempo.minuto >= 60) {
     (tempo.minuto -= 60), tempo.hora++;
-    console.log(`Vida: ${personagem.vida}`);
   }
+  console.log(`Dia ${tempo.dia} - ${tempo.hora}h:${tempo.minuto}m`);
 };
 descanso = () => {
   if (personagem.vida < 15) {
@@ -123,9 +123,9 @@ escolhaDestino = () => {
     console.clear();
     mostraStatus();
     console.log(`Por onde você deseja ir? `);
-    let listaDestino = [, `(1) Resgatar a princesa Arlim`, `(2) Roubar a espada Lívera`, `(3) Enfrentar o dragão Míssera`, `(4) Buscar a pedra mística de Labuge`, `(5) Dormir`, , , , `(9) Desistir`];
+    let listaDestino = [, `(1) Resgatar a princesa Arlim`, `(2) Roubar a espada Lívera`, `(3) Buscar a pedra mística de Labuge`, `(4) Enfrentar o dragão Míssera`, `(5) Dormir`, , , , `(9) Desistir`];
 
-    if (princesa < 1) {
+    if (arlim < 1) {
       console.log(listaDestino[1]);
     }
     if (livera < 1) {
@@ -160,7 +160,7 @@ while (true) {
     // ALEATORIEDADE
     emboscada = dado();
 
-    if (emboscada % 2 == 0) {
+    if (emboscada % 2 == 0 && livera == 0) {
       console.clear();
       quantidadeInimigos = Math.floor(Math.random() * 3 + 2);
       console.clear();
@@ -173,7 +173,7 @@ while (true) {
         do {
           escolha = +prompt(`Deseja (1) ATACAR ou (2) DEFENDER: `);
           console.clear();
-        } while (isNaN(destino) || !Number.isInteger(destino) || destino < 1 || destino > 2);
+        } while (isNaN(escolha) || !Number.isInteger(escolha) || escolha < 1 || escolha > 2);
 
         jogada = dado();
         emboscada = dado();
@@ -200,13 +200,13 @@ while (true) {
           }
         }
         if (personagem.vida <= 0) {
-          console.clear();
-          gameOver();
           break;
         }
       }
       if (personagem.vida <= 0) {
         console.clear();
+        mostraTempo();
+        console.log(`Você lutou bravamente mas, acabou morto.`);
         gameOver();
         break;
       }
@@ -214,12 +214,21 @@ while (true) {
       console.clear();
       mostraStatus();
       console.log(`Você derrotou todos os inimigos.`);
-
+      continuar();
       descanso();
-
+      passaHora(1);
+    } else if (emboscada % 2 == 0 && livera != 0) {
+      quantidadeInimigos = Math.floor(Math.random() * 3 + 2);
+      passaHora(1);
+      mostraStatus();
+      console.log(`Emboscada!`);
+      console.log(`Você deu de cara com ${quantidadeInimigos} Argadans.`);
+      continuar();
+      mostraStatus();
+      console.log(`Quando os guardas avistaram você em posse da Lívera, tentaram fugir. Você não deixou, é claro, e acabou com todos eles.`);
+      continuar();
       passaHora(1);
     } else {
-      console.clear;
       passaHora(2);
     }
 
@@ -241,13 +250,23 @@ while (true) {
       console.log(`Juntos conseguiram fugir e voltaram para a cidade!`);
       console.log();
       continuar();
-      princesa++;
+      arlim++;
     } else if (escolha == 2) {
-      console.clear();
-      mostraTempo();
-      console.log(`Você travou um intensa batalha contra os Argadans, mas eles eram muitos e você acabou morto!`);
-      gameOver();
-      break;
+      if (livera == 0) {
+        console.clear();
+        mostraTempo();
+        console.log(`Você travou um intensa batalha contra os Argadans, mas eles eram muitos e você acabou morto!`);
+        gameOver();
+        break;
+      } else {
+        mostraStatus();
+        passaHora(2);
+        passaMinuto(30);
+        console.log(`Com a Lívera em mãos, os guardas não tiveram chance contra você, que derrotou todos os inimigos`);
+        console.log(`e salvou Arlim da prisão, voltando com ela para a cidade.`);
+        continuar();
+        arlim++;
+      }
     } else {
       jogada = dado();
       if (jogada % 2 == 0) {
@@ -257,7 +276,7 @@ while (true) {
         console.log(`Você conseguiu resgatar a princesa sem que ninguém percebesse. Juntos, vocês voltaram pra cidade...`);
         console.log();
         continuar();
-        princesa++;
+        arlim++;
       } else {
         do {
           console.clear();
@@ -284,7 +303,7 @@ while (true) {
           console.log(`Juntos, vocês conseguem fugir e voltar a cidade.`);
           console.log();
           continuar();
-          princesa++;
+          arlim++;
         } else {
           console.clear();
           passaHora(1);
@@ -304,31 +323,152 @@ while (true) {
     continuar();
     manada = dado();
     if (manada % 2 == 0) {
+      passaHora(1);
+      passaMinuto(30);
       mostraStatus();
       console.log(`Enquanto atravessava um imenso vale, você avistou uma manada de Orlus, animais maiores que elefantes.`);
-      console.log(`Você precisa decidir como agir...`);
+      console.log(`Você precisa decidir como agir.`);
       continuar();
-      mostraStatus();
-      console.log(`Pode tentar correr mais que os Orlus e voltar ao inicio do vale, à cerca de 1km...`);
-      console.log(`Pode se esconder atrás de uma enorme pedra no meio do vale....`);
-      console.log(`Subir em uma árvore também no meio do vale...`);
-      console.log();
-      console.log(`(1) CORRER PARA O INICIO DO VALE`);
-      console.log(`(2) ESCONDER ATRÁS DA PEDRA`);
-      console.log(`(3) SUBIR NA ÁRVORE`);
-      escolha = +prompt();
+      do {
+        mostraStatus();
+        console.log(`Pode subir em uma árvore no meio do vale...`);
+        console.log(`Você pode tentar correr, antes que os Orlus te alcance, e voltar ao inicio do vale, à cerca de 1km...`);
+        console.log(`Pode se esconder atrás de uma enorme pedra também no meio do vale....`);
+        console.log();
+        console.log(`(1) SUBIR NA ÁRVORE`);
+        console.log(`(2) CORRER PARA O INICIO DO VALE`);
+        console.log(`(3) ESCONDER ATRÁS DA PEDRA`);
+        escolha = +prompt();
+      } while (isNaN(escolha) || !Number.isInteger(escolha) || escolha < 1 || escolha > 3);
+
       if (escolha == 1) {
-        addminuto(10);
+        manada = dado();
+        if (manada % 2 == 0) {
+          passaMinuto(20);
+          mostraTempo();
+          console.log(`Você subiu na árvore. A manada desviou e por pouco não derrubaram a árvore.`);
+          continuar();
+        } else {
+          passaMinuto(10);
+          mostraTempo();
+          console.log(`Você subiu na árvore... mas, a manada acabou derrubando a árvore e você foi pisoteado.`);
+          gameOver();
+          break;
+        }
+      } else if (escolha == 2) {
+        passaMinuto(10);
         mostraTempo();
         console.log(`Você correu muito rápido... só não, mais rápido que a manada que te pisoteou.`);
+        gameOver();
+        break;
+      } else if (escolha == 3) {
+        passaMinuto(20);
+        mostraStatus();
+        console.log(`Você chegou a pedra. A manada acabou desviando da pedra e você ficou protegido.`);
+        continuar();
+      }
+      passaHora(1);
+      passaMinuto(30);
+    } else {
+      passaHora(3);
+    }
+    do {
+      mostraStatus();
+      console.log(`Você chegou a base! Há tantos guardas que você não conseguiu contar todos. Parou no 50.`);
+      console.log(`Como você deseja agir?`);
+      console.log();
+      console.log(`(1) Atacar quem aparecer pela frente...`);
+      console.log(`(2) Dar uma volta, buscando um meio de entrar sem ser visto...`);
+      console.log(`(3) Se entregar e torcer pra ser levado para próximo da espada e então planejar algo para roubá-la...`);
+      escolha = +prompt();
+    } while (isNaN(escolha) || !Number.isInteger(escolha) || escolha < 1 || escolha > 3);
+    if (escolha == 1) {
+      mostraTempo();
+      console.log(`Corajoso da sua parte, devo admitir. Mas, sem chance de derrotar mais de 50 Argadans sem a espada de Lívera.`);
+      gameOver();
+      break;
+    } else if (escolha == 2) {
+      passaMinuto(40);
+      mostraStatus();
+      quantidadeInimigos = Math.floor(Math.random() * 4 + 2);
+      console.log(`Ao dar a volta, você achou uma brecha entre as pedras do morro, que permitiu que você entrasse até o local da espada.`);
+      console.log(`Há ${quantidadeInimigos} guardas que você terá que lutar!`);
+      console.log();
+
+      for (i = quantidadeInimigos; i >= 1; passaMinuto(5)) {
+        do {
+          escolha = +prompt(`Deseja (1) ATACAR ou (2) DEFENDER: `);
+          console.clear();
+        } while (isNaN(escolha) || !Number.isInteger(escolha) || escolha < 1 || escolha > 2);
+
+        jogada = dado();
+        emboscada = dado();
+
+        if (escolha == 1) {
+          if (jogada >= emboscada) {
+            i--;
+            mostraStatus();
+            console.log(`Você derrotou um inimigo, falta ${i}`);
+          } else {
+            perdeVida(2);
+            mostraStatus();
+            console.log(`O inimigo defendeu seu ataque e contra atacou!`);
+          }
+        }
+        if (escolha == 2) {
+          if (jogada >= emboscada) {
+            mostraStatus();
+            console.log(`Você defendeu o ataque inimigo.`);
+          } else {
+            perdeVida(2);
+            mostraStatus();
+            console.log(`Você recebeu um ataque!`);
+          }
+        }
+        if (personagem.vida <= 0) {
+          break;
+        }
+      }
+      if (personagem.vida <= 0) {
+        console.clear();
+        mostraTempo();
+        console.log(`Mesmo com toda sua bravura, você acabou morto.`);
+        gameOver();
+        break;
+      }
+
+      mostraStatus();
+      passaHora(3);
+      passaMinuto(30);
+      console.log(`Você derrotou os os guardas que vigiavam a espada. Em posse da Lívera, você derrotou todos os Argadans da base e voltou para a cidade.`);
+      continuar();
+      livera++;
+      descanso();
+    } else if (escolha == 3) {
+      jogada = dado();
+      if (jogada % 2 == 0) {
+        mostraStatus();
+        passaMinuto(15);
+        console.log(`Você se entregou. Quando os guardas te levava para a prisão, você avistou a espada e num vacilo dos guardas, você conseguiu pegá-la.`);
+        continuar();
+        mostraStatus();
+        passaHora(3);
+        passaMinuto(30);
+        console.log(`Com a Lívera em mãos, você derrotou todos os guardas da base e voltou para a cidade.`);
+        continuar();
+        livera++;
+      } else {
+        passaMinuto(15);
+        mostraTempo();
+        console.log(`Você se entregou e foi para a prisão. Lá, não conseguiu encontrar nenhuma maneira de escapar e roubar a espada. Ficará preso até a morte...`);
         gameOver();
         break;
       }
     }
   }
 
-  // DESTINO 3
-  else if (destino == 3) {
+  // DESTINO 4
+  else if (destino == 4) {
     if (livera == 0 && labuge == 0) {
       do {
         mostraStatus();
