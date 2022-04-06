5cd3de9caa9bb15a6a287f1f97ea323bbe7662f6
Kleiton Lima <kleiton.mini@gmail.com>
2022-04-06 00:01:14 -0300

    Update final!

--- Módulo 1/M1 - Projeto 03 - Jogo de Ficção Interativa .js
+++ Módulo 1/M1 - Projeto 03 - Jogo de Ficção Interativa .js
@@ -25,6 +25,7 @@ let destino,
   emboscada,
   escolha,
   jogada,
+  inimigos,
   quantidadeInimigos,
   manada,
   gerald,
@@ -352,7 +353,10 @@ while (true) {
       statusPers();
       console.log(`Perfeito! Desça por esse caminho e chegará a base dos Argadans, onde a espada está localizada.`);
       continuar();
+
+      // ALEATORIEDADE
       manada = dado();
+
       if (manada % 2 == 0) {
         passaHora(1);
         passaMinuto(30);
@@ -364,7 +368,7 @@ while (true) {
           statusPers();
           listaEscolha = [, `(1) SUBIR NA ÁRVORE`, `(2) CORRER PARA O INICIO DO VALE`, `(3) ESCONDER ATRÁS DA PEDRA`];
           console.log(`Pode subir em uma árvore no meio do vale...`);
-          console.log(`Você pode tentar correr, antes que os Orlus te alcance, e voltar ao inicio do vale, à cerca de 1km...`);
+          console.log(`Você pode tentar correr, antes que os Orlus te alcance e voltar ao inicio do vale, à cerca de 1km...`);
           console.log(`Pode se esconder atrás de uma enorme pedra também no meio do vale....`);
           console.log();
           console.log(listaEscolha[1]);
@@ -374,15 +378,16 @@ while (true) {
         } while (isNaN(escolha) || !Number.isInteger(escolha) || escolha < 1 || escolha > 3);
 
         if (escolha == 1) {
+          // PROBABILIDADE DE DAR CERTO OU NÃO
           manada = dado();
           if (manada % 2 == 0) {
             passaMinuto(20);
-            mostraTempo();
+            statusPers();
             console.log(`Você subiu na árvore. A manada desviou e por pouco não derrubaram a árvore.`);
             continuar();
           } else {
             passaMinuto(10);
-            mostraTempo();
+            statusPers();
             console.log(`Você subiu na árvore... mas, a manada acabou derrubando a árvore e você foi pisoteado.`);
             gameOver();
             break;
@@ -390,13 +395,13 @@ while (true) {
         } else if (escolha == 2) {
           passaMinuto(10);
           mostraTempo();
-          console.log(`Você correu muito rápido... só não, mais rápido que a manada que te pisoteou.`);
+          console.log(`Você correu muito rápido... só não mais rápido que a manada que te pisoteou.`);
           gameOver();
           break;
         } else if (escolha == 3) {
           passaMinuto(20);
           statusPers();
-          console.log(`Você chegou a pedra. A manada acabou desviando da pedra e você ficou protegido.`);
+          console.log(`Você chegou a pedra e se escondeu atrás dela. A manada acabou desviando da pedra e você ficou protegido.`);
           continuar();
         }
         passaHora(1);
@@ -431,9 +436,8 @@ while (true) {
         quantidadeInimigos = Math.floor(Math.random() * 4 + 2);
         console.log(`Ao dar a volta, você achou uma brecha entre as pedras do morro, que permitiu que você entrasse até o local da espada.`);
         console.log(`Há ${quantidadeInimigos} guardas que você terá que lutar!`);
-        console.log();
 
-        for (i = quantidadeInimigos; i >= 1; passaMinuto(5)) {
+        for (i = quantidadeInimigos; i > 0; passaMinuto(5)) {
           do {
             console.log();
             escolha = +prompt(`Deseja (1) ATACAR ou (2) DEFENDER: `);
@@ -476,23 +480,23 @@ while (true) {
           break;
         }
 
-        statusPers();
         passaHora(3);
         passaMinuto(30);
+        statusPers();
         console.log(`Você derrotou os guardas que vigiavam a espada. Em posse da Lívera, você derrotou todos os Argadans da base e voltou para a cidade.`);
         continuar();
         livera++;
-        descanso();
       } else if (escolha == 3) {
+        // POSSIBILIDADE DE DAR CERTO OU NÃO
         jogada = dado();
         if (jogada % 2 == 0) {
-          statusPers();
           passaMinuto(15);
-          console.log(`Você se entregou. Quando os guardas te levava para a prisão, você avistou a espada e num vacilo dos guardas, você conseguiu pegá-la.`);
-          continuar();
           statusPers();
+          console.log(`Você se entregou. Quando os guardas te levava para a prisão, você avistou a espada e num vacilo dos guardas, conseguiu pegá-la.`);
+          continuar();
           passaHora(3);
           passaMinuto(30);
+          statusPers();
           console.log(`Com a Lívera em mãos, você derrotou todos os guardas da base e voltou para a cidade.`);
           continuar();
           livera++;
@@ -508,20 +512,22 @@ while (true) {
   } else if (destino == 3) {
     if (labuge > 0) {
       statusPers();
-      console.log(`Você já encontrou a pedra de Labuge! Siga nas outras missões.`);
+      console.log(`Você já recuperou a pedra de Labuge! Siga nas outras missões.`);
       continuar();
     } else if (arlim == 0) {
       do {
         statusPers();
         console.log(`A princesa Arlim é a única conhecedora do caminho correto para encontrar a pedra de Labuge.`);
-        console.log(`Sem ela, será pior que procurar uma agulha em um palheiro. Deseja prosseguir, mesmo assim?`);
+        console.log(`Sem ela, será pior que procurar uma agulha em um palheiro.`);
+        console.log(`Deseja prosseguir, mesmo assim?`);
         escolha = prompt().toLowerCase();
       } while (escolha != `s` && escolha != `n` && escolha != `sim` && escolha != `nao`);
+
       if (escolha == `s` || escolha == `sim`) {
         statusPers();
         console.log(`Muito bem! Se prefere assim, não vou impedi-lo.`);
         console.log(`Siga por dentro da floresta, até o precipício. Dizem que a pedra pode ser encontrada nas paredes desse precipício.`);
-        console.log(`Ninguém nunca voltou vivo pra contar a história se é verdade. Boa sorte!`);
+        console.log(`Ninguém nunca voltou vivo pra contar se a história é verdade. Boa sorte!`);
         continuar();
         passaHora(4);
         statusPers();
@@ -537,10 +543,8 @@ while (true) {
       statusPers();
       console.log(`Chegando a beira do precipício, a princesa começa a se guiar de uma maneira totalmente inacreditável,`);
       console.log(`até mesmo pra você um explorador tão habilidoso.`);
-      continuar();
       passaHora(1);
       passaMinuto(30);
-      statusPers();
       console.log(`Pouco a pouco, vocês vão descendo pela difícil parede do precipício, parando as vezes para a princesa se guiar,`);
       console.log(`seja lá como ela estivesse fazendo isso!`);
       continuar();
@@ -548,8 +552,6 @@ while (true) {
       statusPers();
       console.log(`Chegando ao fim do precipício, guiados, no meio da escuridão, por uma tocha que você acendeu,`);
       console.log(`vocês chegam a entrada da caverna onde a princesa disse estar a pedra.`);
-      continuar();
-      statusPers();
       console.log(`Você fica fascinado com as escritas e desenhos nas paredes e segue passando a mão nelas, enquanto a princesa segue mais adiante.`);
       console.log(`Enquanto passava a mão nas paredes, você acaba tocando em uma Eslife, uma animal parecido com uma cobra e ela morde sua mão esquerda.`);
       continuar();
@@ -558,7 +560,7 @@ while (true) {
       statusPers();
       console.log(`Você precisa decidir rápido o que fazer:`);
       console.log(`Pode arrancar a mão com o facão pra evitar que, se houver veneno, se espalhe pelo corpo.`);
-      console.log(`Chupar o local da mordida e cuspir o possivel veneno ou correr em busca dá princesa e pedir ajuda.`);
+      console.log(`Chupar o local da mordida e cuspir o possível veneno ou correr em busca da princesa e pedir ajuda.`);
       continuar();
       do {
         statusPers();
@@ -569,11 +571,12 @@ while (true) {
         console.log(listaEscolha[3]);
         escolha = +prompt();
       } while (isNaN(escolha) || !Number.isInteger(escolha) || escolha < 1 || escolha > 3);
+
       if (escolha == 1) {
-        statusPers();
         passaMinuto(15);
+        statusPers();
         console.log(`Você amarrar um pedaço de pano no ante braço, apoia o braço na pedra e com o facão arranca a mão!`);
-        console.log(`A princesa escuta seu grito de dor e corre ao seu encontro. Ao ver a picada em sua mão no chão ela diz que a Eslife não é um veneno mortal`);
+        console.log(`A princesa escuta seu grito de dor e corre ao seu encontro. Ao ver a picada em sua mão no chão ela diz que a Eslife não tem um veneno mortal`);
         console.log(`e bastava apenas amarrar um pano no braço para evitar que a substância se espalha-se no corpo e causa-se alucinações.`);
         continuar();
         statusPers();
@@ -581,57 +584,50 @@ while (true) {
         continuar();
         passaHora(1);
         mostraTempo();
-        console.log(`Na subida do penhasco, você se desequilibra e, sem a outra mão, não consegue se segurar e acaba caindo e morrendo...`);
+        console.log(`Na subida do penhasco, você se desequilibra, sem a outra mão, não consegue se segurar e acaba caindo e morrendo...`);
         gameOver();
         break;
       } else if (escolha == 2) {
+        // POSSIBILIDADE DE DAR CERTO OU NÃO
         jogada = dado();
         if (jogada % 2 == 0) {
           statusPers();
-          console.log(`Você começa a chupar o local da mordida e cuspir. Repetidas vezes você repete isso...`);
+          console.log(`Você começa a chupar o local da mordida e cuspir. Repetidas vezes você faz isso...`);
           console.log(`De repente, as coisas começam a girar. Atordoado, você se segura na pedra`);
-          continuar();
-          statusPers();
           console.log(`Você avista uma criatura estranha se aproximando. Com o facão na mão você acerta a criatura que caí no chão.`);
           console.log(`Você fica ali parado até que tudo pare de girar. Ao observar novamente a criatura no chão, você vê que se tratava da princesa Arlim.`);
           continuar();
           mostraTempo();
-          console.log(`A substância da Eslife devia ser alucinógena e fez você enxergar a princesa de outra forma!.`);
+          console.log(`A substância da Eslife devia ser alucinógena e fez você enxergar a princesa de outra forma.`);
           console.log(`Com ela morta, você segue a busca sozinho mas, a língua escrita nas parede é totalmente desconhecida.`);
           console.log(`Você fica perdido nos labirintos, até a morte...`);
           gameOver();
           break;
         } else {
-          statusPers();
           passaMinuto(10);
+          statusPers();
           console.log(`Você começa a chupar o local do ferimento. A princesa logo nota sua falta e volta ao seu encontro.`);
           console.log(`Ela manda você parar e amarra, muito apertado, um pano em seu braço. O veneno da Eslife não é fatal e sim alucinógeno.`);
-          continuar();
-          statusPers();
           console.log(`Em cerca de 1 hora o efeito deve passar. Com o pano amarrado no braço a substância não irá se espalhar pelo corpo. Assim, vocês voltam a busca.`);
           continuar();
         }
       } else if (escolha == 3) {
         statusPers();
         console.log(`Você vai ao encontro da princesa. Mostrando a ela o ferimento e descrevendo o animal, ela amarra um pano em seu braço, apertado muito forte.`);
-        console.log(`O veneno da Eslife não é fatal, apenas alucinógeno. Assim, vocês continuam a busca.`);
+        console.log(`Em cerca de 1 hora o efeito deve passar. Com o pano amarrado no braço a substância não irá se espalhar pelo corpo. Assim, vocês voltam a busca.`);
         continuar();
       }
       descanso();
-      statusPers();
       passaHora(3);
-      console.log(`Horas depois de entrar na caverna, que é um labirinto que muda sempre, seguindo as escritas que a princesa foi traduzindo, vocês conseguem chegar ao final.`);
-      continuar();
       statusPers();
+      console.log(`Horas depois de entrar na caverna, que é um labirinto que muda sempre, seguindo as escritas que a princesa foi traduzindo, vocês conseguem chegar ao final.`);
       console.log(`No local, vocês avistam a pedra de Labuge, em um totem antigo. Na frente dele, 3 pequenos pedestais, cada um com um item diferente.`);
       continuar();
       statusPers();
       console.log(`No primeiro pedestal, uma moeda de ouro. No segundo uma maçã. No terceiro um pequeno jarro de barro com água.`);
       console.log(`Entre os pedestais e o totem, uma estátua de um ser humano ajoelhado, com as mãos juntas e estendidas para frente como se pedisse algo.`);
-      continuar();
-      statusPers();
-      console.log(`A princesa lê o que está escrito na parede atrás: "Para a pedra de Labuge ter, é necessário saber o que é mais essencial para viver"`);
-      console.log(`A moeda significa riqueza. A maçã alimento. A água ela mesma. Você deve entregar a estátua o correto para que a pedra seja entregue.`);
+      console.log(`A princesa lê o que está escrito na parede atrás: "Para a pedra de Labuge ter, é necessário saber o que é o mais essencial para viver"`);
+      console.log(`A moeda significa riqueza. A maçã alimento. A água, ela mesma. Você deve entregar a estátua o correto para que a pedra seja entregue.`);
       console.log(`Caso contrário, tudo irá desabar e aqui, enterrados, ficaremos para sempre.`);
       continuar();
       do {
@@ -643,12 +639,12 @@ while (true) {
         console.log(listaEscolha[2]);
         console.log(listaEscolha[3]);
         escolha = +prompt();
-        console.log();
       } while (isNaN(escolha) || !Number.isInteger(escolha) || escolha < 1 || escolha > 3);
+
       if (escolha == 1) {
         passaMinuto(5);
         mostraTempo();
-        console.log(`Você pega moeda e coloca na mão da estátua.`);
+        console.log(`Você pega a moeda de ouro e coloca na mão da estátua.`);
         console.log(`Tudo começou a tremer, a passagem por onde vocês entraram se fechou a estátua pegou fogo e tudo desmoronou.`);
         gameOver();
         break;
@@ -656,18 +652,16 @@ while (true) {
         passaMinuto(5);
         mostraTempo();
         console.log(`Você pega a maçã e coloca na mão da estátua.`);
-        console.log(`A estátua começou a virar pó. Tudo ao redor começou a virar pó, enterrando vocês vivos!`);
+        console.log(`A estátua começou a virar pó. Tudo ao redor começa a virar pó, enterrando vocês vivos!`);
         gameOver();
         break;
       } else if (escolha == 3) {
         passaMinuto(5);
         statusPers();
         console.log(`Você coloca o pequeno jarro com água na mão da estátua.`);
-        console.log(`Começam a nascer flores na estátua que se alastram por todo o lado deixando tudo cheio de flores.`);
-        console.log(`Uma enorme flor nasce do totem e trás até você a pedra de Labuge. Ao desabrochar ela revela a pedra de Labuge dentro dela.`);
-        continuar();
+        console.log(`Começam a nascer flores na estátua que se alastram por todo o lado deixando tudo bem florido.`);
+        console.log(`Uma enorme flor nasce do totem e vem até você. Ao desabrochar ela revela a pedra de Labuge dentro dela.`);
         passaMinuto(5);
-        statusPers();
         console.log(`Ao pegar a pedra, as paredes atrás do totem se abrem, revelando uma saída.`);
         console.log(`Juntos e com a pedra de Labuge, vocês voltam para cidade!`);
         continuar();
@@ -686,24 +680,21 @@ while (true) {
       console.log(`Perfeito! Você está pronto para derrotar o Míssera! Seja forte e corajoso nessa batalha.`);
       console.log(`Suba por este caminho e chegará ao covil do dragão Míssera! Boa sorte!`);
       continuar();
-
       passaHora(5);
       statusPers();
       console.log(`Depois de horas subindo a montanha, finalmente você chega na caverna do dragão Míssera.`);
       passaMinuto(20);
-      console.log(`Você entra devagar caverna adentro. Então, você percebe que, por dentro, mais parece um castelo.`);
+      console.log(`Você entra devagar caverna adentro. Você percebe que, por dentro, a caverna mais parece um castelo.`);
       console.log(`Com alguns candelabros e umas fogueiras acesas você vai se guiando e entrando ainda mais.`);
       continuar();
       statusPers();
       console.log(`Rápido e repentinamente atrás de você, saindo das sombras uma boca gigante e aberta vem em sua direção.`);
-      console.log(`Com a pedra e Labuge, você cria uma bola escudo ao seu redor que te protege da mordida mortal do Míssera.`);
-      continuar();
-      statusPers();
+      console.log(`Com a pedra e Labuge, você cria uma bolha escudo ao seu redor que te protege da mordida mortal do Míssera.`);
       console.log(`Com a Lívera você fura a língua do Míssera, que se afasta e se levanta dentro da sua caverna.`);
       console.log(`Você contempla a grandiosidade do Míssera, enquanto prepara a espada de Lívera e a pedra de Labuge para a batalha!`);
       continuar();
 
-      batalha: while (true) {
+      while (true) {
         missera = dado();
         gerald = dado();
         do {
@@ -720,14 +711,12 @@ while (true) {
             missera += dragao.defesa;
             perdeVidaDrag(gerald - missera);
             statusPers();
-
             console.log(`Você acertou o ataque!`);
             statusDrag();
             continuar();
           } else {
             perdeVidaPers(missera - gerald);
             statusPers();
-
             console.log(`Míssera defendeu o ataque e contra-atacou!`);
             statusDrag();
             continuar();
@@ -744,13 +733,11 @@ while (true) {
             missera += dragao.ataque;
             perdeVidaPers(missera - gerald);
             statusPers();
-
             console.log(`Você recebeu um ataque!`);
             statusDrag();
             continuar();
           } else {
             statusPers();
-
             console.log(`Você defendeu o ataque!`);
             statusDrag();
             continuar();
@@ -765,7 +752,7 @@ while (true) {
       }
       if (dragao.vida <= 0) {
         statusPers();
-        console.log(`Você venceu o dragão Míssera! Agora a dimensão Uruzar está livre e em paz!`);
+        console.log(`Você venceu o dragão Míssera! Agora a dimensão Uruzar está livre e poderá viver em paz para sempre! Obrigado!`);
         continuar();
       }
       if (personagem.vida <= 0) {
@@ -774,7 +761,7 @@ while (true) {
         gameOver();
         break;
       }
-      passaHora(2);
+      passaHora(5);
     }
   } else if (destino == 5) {
     if (missera == 0) {
@@ -783,8 +770,9 @@ while (true) {
         console.log(`Se você for agora, sem derrotar o dragão Míssera, tudo que fez até agora será em vão para nós, mas, pelo menos você voltará para seu lar!`);
         escolha = prompt(`Deseja voltar para a terra, mesmo assim? `).toLowerCase();
       } while (escolha != `s` && escolha != `sim` && escolha != `n` && escolha != `nao`);
+
       if (escolha == `s` || escolha == `sim`) {
-        console.clear();
+        mostraTempo();
         console.log(`Aos prantos, Arlim te orienta a colocar a pedra de Labuge no chão e atingi-la com a ponta da espada de Lívera.`);
         console.log(`Fazendo isso um imenso brilho saí da pedra, até que não seja mais possível enxergar nada além da luz.`);
         console.log(`Você acorda no mesmo local onde foi sugado. No lugar de onde antes havia o portal, não há mais nada além da rocha.`);
@@ -794,7 +782,7 @@ while (true) {
         break;
       }
     } else {
-      console.clear();
+      mostraTempo()
       console.log(`A dimensão Uruzar agradece por tudo que fez por nós! Seremos eternamente gratos a você!`);
       continuar();
       console.clear();
