ad5a6f486beb4c9e256c56856faef836c11851c3
Kleiton Lima <kleiton.mini@gmail.com>
2022-04-14 22:43:11 -0300

    Inclusão das imagens

---
+++ Módulo 2/Projeto 1 - Jogo de humor/scripts.js
@@ -0,0 +1,10 @@
+const imgslist = ['https://uploads.jovemnerd.com.br/wp-content/uploads/2021/08/matrix-4.jpg', 
+'./assets/img/1 ano na programação - voando.gif',
+'./3 dias na programação - mais bala.gif',
+'./assets/img/10 dias na programação - vendo código em tudo.gif',
+'./assets/img/15 dias na programação - começou a pegar o jeito.gif',
+'./assets/img/1 mês na programação - pegando agilidade e se livrando dos erros.gif',
+'./assets/img/3 meses na programação - parando os erros sem muito esforço.gif',
+'./assets/img/1 ano na programação - voando.gif'
+];
+
